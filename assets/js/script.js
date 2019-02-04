9379de885f83d932cdf283c879c4af9e67a8553f
Romain Levasseur <17664@csmrouen.net>
2019-02-04 11:58:38 +0100

    Ajout attribut src (decouplage de code) if, else if

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+var message = prompt('Veuillez saisir une valeur');
+var reponse = ""
+
+if (message < 0) {
+  reponse = 'Nombre négatif';
+}
+
+else if (message > 0) {
+  reponse = 'Nombre positif';
+}
+else {
+  reponse = 'Null';
+}
+
+alert(reponse);
