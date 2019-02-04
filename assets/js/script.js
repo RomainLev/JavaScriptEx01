59bcda001f049bfbdb25c442bc85a233e4bc3fa1
Romain Levasseur <17664@csmrouen.net>
2019-02-04 15:27:30 +0100

    compte + boucle dans boucle

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,21 @@
 var resultat = "<ul>";
+var compte = 0
 
-for (var i = -500 ; i <= 724 ; i += 10) {
-  if (i % 13 == 0) {
-  resultat += "<li>" + i + "</li>";
+for (var i = -12728 ; i < 16857 ; i += 17) {
+  if (i % 23 == 0) {
+    compte ++;
+    resultat += "<li>" + i;
+
+  resultat += "<ul>";
+  for (var j = 0 ; j < 11 ; j++){
+    resultat += "<li>" + j + "</li>";
+    }
+    resultat += "</ul>";
+    resultat += "</li>";
   }
 }
 
-resultat = resultat +"</ul>";
+resultat += "</ul>";
 
 document.write(resultat);
+alert(compte);
