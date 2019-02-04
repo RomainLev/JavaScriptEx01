9b9c78de42e91f8589df7d2cb20ec9244367b459
Romain Levasseur <17664@csmrouen.net>
2019-02-04 17:03:23 +0100

    calculer la racine carrée de l'hypo (a corriger)

---
+++ assets/js/fonction.js
@@ -0,0 +1,8 @@
+function calculerCarre(toto) {
+   let miseaucarre = toto ** 2;
+   return miseaucarre;
+ }
+
+function calculerHypo(a, b) {
+  return resultat = Math.sqrt(a ** 2 + b ** 2) ;
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,4 @@
-var resultat = "<ul>";
-var compte = 0
+var a = prompt("Veuillez saisir un des cotés qui n'est pas l'hypotenuse")
+var b = prompt("Veuillez saisir l'autre coté qui n'est pas l'hypotenuse")
 
-for (var i = -12728 ; i < 16857 ; i += 17) {
-  if (i % 23 == 0) {
-    compte ++;
-    resultat += "<li>" + i;
-
-  resultat += "<ul>";
-  for (var j = 0 ; j < 11 ; j++){
-    resultat += "<li>" + j + "</li>";
-    }
-    resultat += "</ul>";
-    resultat += "</li>";
-  }
-}
-
-resultat += "</ul>";
-
-document.write(resultat);
-alert(compte);
+alert resultat;
